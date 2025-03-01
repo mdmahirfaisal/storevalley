8458362c494867e168931c7fa27432899ab9101a
MD Mahir Faisal <mahir.faisal@koneksys.com>
2025-03-01 07:08:28 +0600

    fixed api route for the single  product route api

--- src/app/(root)/products/page.tsx
+++ src/app/(root)/products/page.tsx
@@ -1,4 +1,8 @@
 export default async function Products() {
+  const products = await fetch(`${process.env.API_URL}/products`).then((res) =>
+    res.json()
+  );
+  console.log(products);
   return (
     <div className="p-5">
       <h1 className="text-3xl">this is products page</h1>
--- src/app/api/v1/products/[id]/route.ts
+++ src/app/api/v1/products/[id]/route.ts
@@ -1,9 +1,6 @@
 // app/api/v1/products/[id]/route.ts
 import { NextResponse } from "next/server";
 import jsonProducts from "@/data/products.json";
-// import { ProductsTypes } from "@/types/products";
-
-// const products: ProductsTypes[] = jsonProducts as ProductsTypes[];
 export async function GET(request: Request) {
   const { pathname } = new URL(request.url);
   const id = pathname.split("/").pop();
