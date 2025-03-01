41887dcc119818bc061fcd3de93147ed9187f91c
MD Mahir Faisal <mahir.faisal@koneksys.com>
2025-03-01 17:39:01 +0600

    fixed product page issue and added image config

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
     <div className="p-5">
       <div className="w-[100%] d-flex justify-end"></div>
       <h1 className="text-3xl">Welcome to the Store Valley. -------</h1>
-      Go to the <Link href={"/dashboard"}>Dashboard</Link>
+      <p>
+        Go to the <Link href={"/dashboard"}>Dashboard</Link>
+      </p>
+      <p>
+        Go to the <Link href={"/products"}>Products</Link>
+      </p>
     </div>
   );
 }
